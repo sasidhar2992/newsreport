2fa8a89a430171d0341360d3c21b31960f0aa220
sasidhar <sasidharreddy2@gmail.com>
2016-08-25 05:37:48 -0400

    final commit

--- Gemfile
+++ Gemfile
@@ -15,7 +15,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # gem 'therubyracer', platforms: :ruby
 gem 'nokogiri'
 
-gem 'rubyzip', '>= 1.0.0' # will load new rubyzip version
+gem 'rubyzip', '>= 1.0.0' 
+# will load new rubyzip version
+gem 'httparty', '~> 0.14.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- app/controllers/unzip.rb
+++ app/controllers/unzip.rb
@@ -1,20 +1,24 @@
+module Unzip
+  require 'uri'
+  require "httparty"
 
-    dir = 'zipfolder';
+  output_dir = "app/assets/xmls" 
 
-    output_dir = "app/assets/xmls" 
-
-    files = Dir.glob("zipfolder/*.zip").each do |item|
-      Zip::File.open(item) do |zip_file|
-        # Handle entries one by one
-        zip_file.each do |entry|
-          # Extract to file/directory/symlink
-          if not File.exists?("app/assets/xmls/#{entry.name}") then 
-              puts "Extracting #{entry.name}.inspect"
-               #entry.extract(dest_file)
-              entry.extract("#{File.expand_path(output_dir)}/#{entry.name}")
-              # Read into memory
-              content = entry.get_input_stream.read
-          end
-        end
+  zipfile = Tempfile.new("file")
+  zipfile.binmode # This might not be necessary depending on the zip file
+  zipfile.write(HTTParty.get("http://feed.omgili.com/5Rh5AMTrc4Pv/mainstream/posts/1471852899675.zip").body)
+  zipfile.close
+  Zip::File.open(zipfile.path) do |zip_file|
+    # Handle entries one by one
+    zip_file.each do |entry|
+    # Extract to file/directory/symlink
+      if not File.exists?("app/assets/xmls/#{entry.name}") then 
+          puts "Extracting #{entry.name}.inspect"
+          #entry.extract(dest_file)
+          entry.extract("#{File.expand_path(output_dir)}/#{entry.name}")
+          # Read into memory
+          content = entry.get_input_stream.read
       end
-    end  
+    end
+  end 
+end
