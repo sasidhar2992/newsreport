5d11735a69b00219710c7e4a8d18ec72108aa824
sasidhar <sasidharreddy2@gmail.com>
2016-08-25 06:00:02 -0400

    final commit

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -10,11 +10,13 @@ class NewsController < ApplicationController
     @hash = []
      
     xfiles = Dir.glob("app/assets/xmls/*.xml").each do |xitem|
+      #iterate through each xmlfile
       unless k>=40
         puts xitem
         path = xitem;
         @hash << Hash.from_xml(File.read(xitem))
           $redis.SADD("NEWS_XML", @hash)
+          #create a redis set with values from the hash instance
         #puts hash.inspect 
       end
       k += 1;
